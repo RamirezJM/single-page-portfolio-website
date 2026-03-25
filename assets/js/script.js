5dd29bdf99cc4a48985961575ab060a2a2be0d03
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-03-24 22:19:27 -0300

    refactor: ordenar código

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@ function addRecommendation() {
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
-
+     showPopup(true)
     // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
+    const element = document.createElement("div");
     element.setAttribute("class","recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
     // Add this element to the end of the list of recommendations
